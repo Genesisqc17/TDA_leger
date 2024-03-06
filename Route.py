c319d065cbeb0f7b5005a91ca32aa0c2b8c15bde
Arzyel <146108908+Arzyel@users.noreply.github.com>
2024-03-06 15:38:51 -0500

    Update Route.py

--- Route.py
+++ Route.py
@@ -1,9 +1,9 @@
 class Route():
-    def __init__(self, parent,PosX,PosY,PosX2,PosY2,PosXZoneCible,PosYZoneCible):
+    def __init__(self, parent, posX1, posY1, posX2, posY2, PosXZoneCible1, PosYZoneCible1, PosXZoneCible2, PosYZoneCible2):
         self.parent = parent
-        self.PosX = PosX
-        self.PosY = PosY
-        self.PosX2 = PosX2
-        self.PosY2 = PosY2
+        self.posX1 = posX1
+        self.posY1 = posY1
+        self.posX2 = posX2
+        self.posY2 = posY2
         self.posXZoneCible = PosXZoneCible
         self.posYZoneCible = PosYZoneCible
