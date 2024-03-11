c082dd4758ef23970622c45822654f11b5e4cf0a
Vincent Fournier <genesisqc@hotmail.ca>
2024-03-11 09:00:56 -0400

    Changes in Vue

--- Tower Defense/Vue.py
+++ Tower Defense/Vue.py
@@ -8,17 +8,17 @@ class Vue():
         self.canevas = None
         self.parent = parent
         self.modele = modele
-        self.niv_wave_text = StringVar()
-        self.chrono_text = StringVar()
-        self.nb_vies = StringVar()
-        self.qte_argent = StringVar()
-        self.cout_upgrade_text = StringVar()
-        self.force_upgrade_text = StringVar()
-        self.etendu_upgrade_text = StringVar()
-        self.nom_tour_text = StringVar()
-        self.force_tour_text = StringVar()
-        self.etendu_tour_text = StringVar()
-        self.force_tour_text = StringVar()
+        #self.niv_wave_text = StringVar()
+        #self.chrono_text = StringVar()
+        #self.nb_vies = StringVar()
+        #self.qte_argent = StringVar()
+        #self.cout_upgrade_text = StringVar()
+        #self.force_upgrade_text = StringVar()
+        #self.etendu_upgrade_text = StringVar()
+        #self.nom_tour_text = StringVar()
+        #self.force_tour_text = StringVar()
+        #self.etendu_tour_text = StringVar()
+        #self.force_tour_text = StringVar()
         self.root = Tk()
         self.root.title("Super Tower Defence 64")
         self.police_label = tkinter.font.Font(family="Terminal", size=14, weight="normal")
@@ -57,6 +57,8 @@ class Vue():
                                               font=self.police_bouton)
         self.regles.pack()
         self.champ_nom.pack()
+        self.login.pack()
+        self.commencer_partie_bouton.pack()
         self.intro_frame.pack()
 
         return self.intro_frame
@@ -69,6 +71,8 @@ class Vue():
         # Frame pour la surface de jeu
         self.jeu_frame = Frame(self.parent_frame)
         self.jeu_frame.pack()
+        self.canevasGame
+
 
         # Frame pour le menu interactif
         self.menu_frame = Frame(self.jeu_frame)
@@ -76,8 +80,10 @@ class Vue():
 
         # Frame pour la vague et le chronomètre
         self.vc_frame = Frame(self.menu_frame)
-        self.niv_vague = Label(self.vc_frame, textvariable=self.niv_wave_text, font=self.police_label)
-        self.timer = Label(self.vc_frame, textvariable=self.chrono_text, font=self.police_label)
+        self.niv_vague = Label(self.vc_frame#, textvariable=self.niv_wave_text
+                               , font=self.police_label)
+        self.timer = Label(self.vc_frame#, textvariable=self.chrono_text
+                           , font=self.police_label)
         self.timer.pack()
         self.niv_vague.pack()
         self.vc_frame.pack()
@@ -87,23 +93,22 @@ class Vue():
         self.tour_frame.pack()
 
         # Boutons pour acheter tours
-        self.proj_bouton = Button(self.tour_frame, text="Tour projectile", command=self.acheter_tour,
-                                  font=self.police_bouton)
-        self.eclair_bouton = Button(self.tour_frame, text="Tour éclair", command=self.acheter_tour,
-                                    font=self.police_bouton)
-        self.poison_bouton = Button(self.tour_frame, text="Tour poison", command=self.acheter_tour,
-                                    font=self.police_bouton)
-        self.laser_bouton = Button(self.tour_frame, text="Tour laser", command=self.acheter_tour,
-                                   font=self.police_bouton)
-        self.laser_bouton.pack()
+        self.proj_bouton = Button(self.tour_frame, text="Tour projectile"#, command=self.acheter_tour
+                                  ,font=self.police_bouton)
+        self.eclair_bouton = Button(self.tour_frame, text="Tour éclair"#, command=self.acheter_tour
+                                    ,font=self.police_bouton)
+        self.poison_bouton = Button(self.tour_frame, text="Tour poison"#, command=self.acheter_tour
+                                    ,font=self.police_bouton)
         self.poison_bouton.pack()
         self.eclair_bouton.pack()
         self.proj_bouton.pack()
 
         # Frame pour la vie et l'argent
         self.va_frame = Frame(self.menu_frame)
-        self.cmp_nb_vies = Label(self.va_frame, textvariable=self.nb_vies, font=self.police_label)
-        self.cmp_argent = Label(self.va_frame, textvariable=self.qte_argent, font=self.police_label)
+        self.cmp_nb_vies = Label(self.va_frame#, textvariable=self.nb_vies
+                                 , font=self.police_label)
+        self.cmp_argent = Label(self.va_frame#, textvariable=self.qte_argent
+                                , font=self.police_label)
         self.cmp_argent.pack()
         self.cmp_nb_vies.pack()
         self.va_frame.pack()
@@ -117,25 +122,29 @@ class Vue():
 
         # Frame pour l'info d'une amélioration
         self.info_upgrade_frame = Frame(self.upgrade_frame)
-        self.cout_upgrade = Label(self.info_upgrade_frame, textvariable=self.cout_upgrade_text,
-                                  font=self.police_label)
-        self.upgrade_force = Label(self.info_upgrade_frame, textvariable=self.force_upgrade_text,
-                                   font=self.police_label)
-        self.upgrade_etendu = Label(self.info_upgrade_frame, textvariable=self.etendu_upgrade_text,
-                                    font=self.police_label)
+        self.cout_upgrade = Label(self.info_upgrade_frame#, textvariable=self.cout_upgrade_text
+                                  ,font=self.police_label)
+        self.upgrade_force = Label(self.info_upgrade_frame#, textvariable=self.force_upgrade_text
+                                    ,font=self.police_label)
+        self.upgrade_etendu = Label(self.info_upgrade_frame#, textvariable=self.etendu_upgrade_text
+                                    ,font=self.police_label)
 
         # Bouton pour améliorer une tour
-        self.upgrade_bouton = Button(self.upgrade_frame, text="Améliorer", command=self.ameliorer_tour,
-                                     font=self.police_bouton)
+        self.upgrade_bouton = Button(self.upgrade_frame, text="Améliorer"#, command=self.ameliorer_tour
+                                     ,font=self.police_bouton)
 
         # Frame pour l'info d'une tour
         self.info_tour = Frame(self.upgrade_frame)
-        self.nom_tour = Label(self.info_tour, textvariable=self.nom_tour_text,
-                              font=self.police_label)
-        self.force_tour = Label(self.info_tour, textvariable=self.force_tour_text,
-                                font=self.police_label)
-        self.etendu_tour = Label(self.info_tour, textvariable=self.etendu_tour_text,
-                                 font=self.police_label)
+        self.nom_tour = Label(self.info_tour#, textvariable=self.nom_tour_text
+                              ,font=self.police_label)
+        self.force_tour = Label(self.info_tour#, textvariable=self.force_tour_text
+                                ,font=self.police_label)
+        self.etendu_tour = Label(self.info_tour#, textvariable=self.etendu_tour_text
+                                ,font=self.police_label)
+
+        return self.upgrade_frame
 
     def creer_game_over(self):
+        self.gameover_frame = Frame(self.root)
+
         return self.gameover_frame
